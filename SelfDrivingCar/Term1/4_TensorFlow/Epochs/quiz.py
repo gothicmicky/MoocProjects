667d3b6a4b0d0823d514995cd2262930a1895764
Sharon Liu <sliu8@apple.com>
2016-12-11 18:55:20 -0800

    updated tf scripts

---
+++ SelfDrivingCar/Term1/4_TensorFlow/Epochs/quiz.py
@@ -0,0 +1,78 @@
+from tensorflow.examples.tutorials.mnist import input_data
+import tensorflow as tf
+import numpy as np
+from helper import batches
+
+learning_rate = 0.001
+n_input = 784  # MNIST data input (img shape: 28*28)
+n_classes = 10  # MNIST total classes (0-9 digits)
+
+# Import MNIST data
+mnist = input_data.read_data_sets('~/datasets/ud730/mnist', one_hot=True)
+
+# The features are already scaled and the data is shuffled
+train_features = mnist.train.images
+test_features = mnist.test.images
+
+train_labels = mnist.train.labels.astype(np.float32)
+test_labels = mnist.test.labels.astype(np.float32)
+
+# Features and Labels
+features = tf.placeholder(tf.float32, [None, n_input])
+labels = tf.placeholder(tf.float32, [None, n_classes])
+
+# Weights & bias
+weights = tf.Variable(tf.random_normal([n_input, n_classes]))
+bias = tf.Variable(tf.random_normal([n_classes]))
+
+# Logits - xW + b
+logits = tf.add(tf.matmul(features, weights), bias)
+
+# Define loss and optimizer
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits, labels))
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate).minimize(cost)
+
+# Calculate accuracy
+correct_prediction = tf.equal(tf.argmax(logits, 1), tf.argmax(labels, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+batch_size = 128
+assert batch_size is not None, 'You must set the batch size'
+epochs = 100
+
+def print_epoch_stats(epoch_i, sess, last_features, last_labels, valid_features, valid_labels):
+    """
+    Print cost and validation accuracy of an epoch
+    """
+    current_cost = sess.run(
+        cost,
+        feed_dict={features: last_features, labels: last_labels})
+    
+    valid_accuracy = sess.run(
+        accuracy,
+        feed_dict={features: valid_features, labels: valid_labels})
+    
+    print('Epoch: {:<4} - Cost: {:<8.3} Valid Accuracy: {:<5.3}'.format(
+        epoch_i,
+        current_cost,
+        valid_accuracy))
+
+init = tf.initialize_all_variables()
+train_batches = batches(batch_size, train_features, train_labels)
+
+with tf.Session() as sess:
+    sess.run(init)
+    # Training cycle
+    for epoch_i in range(epochs):
+        for batch_features, batch_labels in train_batches:
+            sess.run(optimizer,feed_dict={features: batch_features, labels: batch_labels})
+
+    # Print cost and validation accuracy of an epoch 
+        print_epoch_stats(epoch_i, sess, batch_features, batch_labels, test_features, test_labels)
+
+    # Calculate accuracy for test dataset
+    test_accuracy = sess.run(
+        accuracy,
+        feed_dict={features: test_features, labels: test_labels})
+
+print('Test Accuracy: {}'.format(test_accuracy))
