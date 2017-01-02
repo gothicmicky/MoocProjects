e8cd60e7ba8e53501afa1e1d5d2751ed35b88e71
Sharon Liu <sliu8@apple.com>
2017-01-01 21:06:46 -0800

    wip

--- SelfDrivingCar/Term1/2_FindingLaneLines/colorSelection.py
+++ SelfDrivingCar/Term1/2_FindingLaneLines/colorSelection.py
@@ -4,7 +4,7 @@ import matplotlib.image as mpimg
 import numpy as np
 
 # Read in the image and print out some stats
-image = (mpimg.imread('test.png')*255).astype('uint8')
+image = (mpimg.imread('test.jpg')*255).astype('uint8')
 print('This image is: ',type(image), 
          'with dimensions:', image.shape)
 
@@ -15,9 +15,9 @@ color_select = np.copy(image)
 
 # Define color selection criteria
 ###### MODIFY THESE VARIABLES TO MAKE YOUR COLOR SELECTION
-red_threshold = 200
-green_threshold = 200
-blue_threshold = 200
+red_threshold = 180
+green_threshold = 180
+blue_threshold = 180
 ######
 
 rgb_threshold = [red_threshold, green_threshold, blue_threshold]
@@ -33,4 +33,4 @@ plt.imshow(color_select)
 plt.imshow(color_select)
 
 # Uncomment the following code if you are running the code locally and wish to save the image
-# mpimg.imsave("test-after.png", color_select)
+mpimg.imsave("test-after.png", color_select)
