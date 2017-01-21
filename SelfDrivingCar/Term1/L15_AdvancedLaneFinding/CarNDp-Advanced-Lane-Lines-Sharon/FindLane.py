f0735116465865de383d2ae4c2cdd8be6723682f
Sharon Liu <sliu8@apple.com>
2017-01-21 11:12:05 -0800

    updated p4

--- SelfDrivingCar/Term1/L15_AdvancedLaneFinding/CarNDp-Advanced-Lane-Lines-Sharon/FindLane.py
+++ SelfDrivingCar/Term1/L15_AdvancedLaneFinding/CarNDp-Advanced-Lane-Lines-Sharon/FindLane.py
@@ -4,6 +4,7 @@ import numpy as np
 import cv2
 import glob
 import os
+import ntpath
 
 '''
 Steps:
@@ -64,36 +65,56 @@ def cal_undistort(img, mtx, dist):
 	undist = cv2.undistort(img, mtx, dist, None, None)
 	return undist
 
+def abs_sobel_thresh(img, orient='x', thresh_min=0, thresh_max=255):
+	# Convert to grayscale
+	gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+	# Apply x or y gradient with the OpenCV Sobel() function
+	# and take the absolute value
+	if orient == 'x':
+		abs_sobel = np.absolute(cv2.Sobel(gray, cv2.CV_64F, 1, 0))
+	if orient == 'y':
+		abs_sobel = np.absolute(cv2.Sobel(gray, cv2.CV_64F, 0, 1))
+	# Rescale back to 8 bit integer
+	scaled_sobel = np.uint8(255*abs_sobel/np.max(abs_sobel))
+	print(scaled_sobel)
+	# Create a copy and apply the threshold
+	binary_output = np.zeros_like(scaled_sobel)
+	# Here I'm using inclusive (>=, <=) thresholds, but exclusive is ok too
+	binary_output[(scaled_sobel >= thresh_min) & (scaled_sobel <= thresh_max)] = 1
+	plt.imshow(binary_output, cmap='gray')
+	print(binary_output)
+	# Return the result
+	return binary_output
+
 
 # Step 1
 # Compute the camera calibration matrix and distortion coefficients given a set of chessboard images.
 mtx, dist = camera_calibration()
 
-
-# Step 2 
-# Apply the distortion correction to the raw image.  
 test_images = glob.glob('test_images/*.jpg')
 for idx, fname in enumerate(test_images):
 	img = cv2.imread(fname)
-	directory = 'output_images/2_distortion_corrected/'
-	if not os.path.exists(directory):
-		os.makedirs(directory)
-	write_name = directory + fname +'.jpg'
-	cv2.imwrite(write_name, img)
-
 	gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 	#printing out some stats and plotting
-	print('This image is:', type(img), 'with dimesions:', img.shape)
+	#print('This image is:', type(img), 'with dimesions:', img.shape)
 	plt.imshow(img) 
+
+	# Step 2: Apply the distortion correction to the raw image.  
 	undist = cal_undistort(img,mtx,dist)
-	print('This image is:', type(undist), 'with dimesions:', undist.shape)
 	plt.imshow(undist) 
 	directory = 'output_images/2_distortion_corrected/'
 	if not os.path.exists(directory):
 		os.makedirs(directory)
-	write_name = directory + fname +'.jpg'
+	write_name = directory + ntpath.basename(fname) +'.jpg'
+	cv2.imwrite(write_name, undist)
 
+	# Step 3: Use color transforms, gradients, etc., to create a thresholded binary image.
+	grad_binary = abs_sobel_thresh(undist, orient='x', thresh_min=10, thresh_max=100)
+	directory = 'output_images/3_color_transforms/'
+	if not os.path.exists(directory):
+		os.makedirs(directory)
+	write_name = directory + ntpath.basename(fname) +'.jpg'
+	cv2.imwrite(write_name, grad_binary)
 
-
-
+	# Step 4: Apply a perspective transform to rectify binary image ("birds-eye view"). 
 
